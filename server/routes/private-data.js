dbd0e3282a377c31e7aef06941ea7b18c8d2c3a1
Steve Larsen <stevelarsen01@gmail.com>
2017-01-05 13:41:19 -0600

    return user object from DB on login

--- server/routes/private-data.js
+++ server/routes/private-data.js
@@ -4,19 +4,19 @@ var pg = require('pg');
 var connectionString = require('../modules/database-config');
 
 router.get("/", function(req, res) {
-    console.log('get route');
     pg.connect(connectionString, function(err, client, done) {
         var userEmail = req.decodedToken.email;
         console.log(userEmail);
-        // Check the user's level of permision based on their email
-        client.query('SELECT clearance_level FROM users WHERE email=$1', [userEmail], function(err, clearanceLevelQueryResult) {
+        /* getting user by email */
+        client.query('SELECT * FROM users WHERE email=$1', [userEmail], function(err, result) {
             done();
             if (err) {
                 console.log('Error COMPLETING clearance_level query task', err);
                 res.sendStatus(500);
             } else {
                 pg.connect(connectionString, function(err, client, done) {
-                    if (clearanceLevelQueryResult.rowCount === 0) {
+                    /* if user does not exist, add to user table */
+                    if (result.rowCount === 0) {
                         client.query(
                             'INSERT INTO users (email) VALUES ($1)', [userEmail],
                             function(err, result) {
@@ -25,13 +25,24 @@ router.get("/", function(req, res) {
                                     console.log('error on user Insert', err);
                                     res.sendStatus(500);
                                 } else {
-                                    console.log('user insert successful for:', userEmail);
-                                    res.sendStatus(200);
+                                    /* Retrieve new user to get id */
+                                    client.query('SELECT * FROM users WHERE email=$1', [userEmail], function(err, result) {
+                                        done();
+                                        if (err) {
+                                            console.log('Error Selecting new user', err);
+                                            res.sendStatus(500);
+                                        } else {
+                                            var currentUser = result.rows[0];
+                                            console.log('new User:', currentUser);
+                                            res.send(currentUser);
+                                        }
+                                    });
                                 }
                             });
                     } else {
-                        var clearanceLevel = clearanceLevelQueryResult.rows[0].clearance_level;
-                        console.log('clearanceLevel:', clearanceLevel);
+                        var currentUser = result.rows[0];
+                        console.log('currentUser:', currentUser);
+                        res.send(currentUser);
                     }
                     done();
                 });

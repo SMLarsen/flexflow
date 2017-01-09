87c21ece742ef4ca67a0e79bacba0f7c09f3c563
emilykeenan <emilyjanekeenan@gmail.com>
2017-01-09 14:33:57 -0600

    added bootstrap and got month buttons plus modal working

--- public/scripts/controller/flow.spend.controller.js
+++ public/scripts/controller/flow.spend.controller.js
@@ -1,3 +1,12 @@
 app.controller('FlowSpendController', ['$http', 'AuthFactory', 'TemplateFactory', function($http, AuthFactory, TemplateFactory) {
   console.log('Flow Spend controller started');
+  var self = this;
+  this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  this.currentMonth = null;
+
+  this.enterMonthFlowData = function(month) {
+    console.log('you have clicked', month);
+    this.currentMonth = month;
+    console.log(this.currentMonth);
+  }
 }]);

da91fdd65547e673fe6b6f187fa07dc1dcc3b6d1
emilykeenan <emilyjanekeenan@gmail.com>
2017-01-09 15:02:44 -0600

    next and previous buttons working

--- public/scripts/controller/flow.spend.controller.js
+++ public/scripts/controller/flow.spend.controller.js
@@ -3,10 +3,31 @@ app.controller('FlowSpendController', ['$http', 'AuthFactory', 'TemplateFactory'
   var self = this;
   this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   this.currentMonth = null;
+  this.currentMonthIndex = null;
 
-  this.enterMonthFlowData = function(month) {
+  this.enterMonthFlowData = function(month, i) {
     console.log('you have clicked', month);
+    console.log(i);
     this.currentMonth = month;
+    this.currentMonthIndex = i;
     console.log(this.currentMonth);
+  }
+
+  this.nextMonth = function() {
+    if(this.currentMonthIndex < 11) {
+    this.currentMonthIndex++;
+  } else {
+    this.currentMonthIndex = 0;
+  }
+    return this.currentMonth = this.months[this.currentMonthIndex];
+  }
+
+  this.prevMonth = function() {
+    if(this.currentMonthIndex > 0) {
+    this.currentMonthIndex--;
+  } else {
+    this.currentMonthIndex = 11;
+  }
+    return this.currentMonth = this.months[this.currentMonthIndex];
   }
 }]);

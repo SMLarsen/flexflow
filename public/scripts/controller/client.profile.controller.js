bd884ac483a4bb9943147445fe244441d5a6e78d
Joseph Giwoyna <jgiwoyna@gmail.com>
2017-01-09 16:45:36 -0600

    added fields for income, take home and budget start

--- public/scripts/controller/client.profile.controller.js
+++ public/scripts/controller/client.profile.controller.js
@@ -1,3 +1,8 @@
 app.controller('ClientProfileController', ['$http', 'AuthFactory', 'TemplateFactory', function($http, AuthFactory, TemplateFactory) {
   console.log('Client Profile controller started');
+
+  var templateFactory = TemplateFactory;
+
+  var self = this;
+
 }]);

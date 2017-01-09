63755b92ebec86fe639f1a2d1bd318e28e9ae4ca
emilykeenan <emilyjanekeenan@gmail.com>
2017-01-09 16:53:28 -0600

    troubleshooting template factory

--- public/scripts/controller/flow.spend.controller.js
+++ public/scripts/controller/flow.spend.controller.js
@@ -1,36 +1,37 @@
 app.controller('FlowSpendController', ['$http', 'AuthFactory', 'TemplateFactory', function($http, AuthFactory, TemplateFactory) {
   console.log('Flow Spend controller started');
   var self = this;
-  this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  this.currentMonth = null;
-  this.currentMonthIndex = null;
-  this.flowCategories = TemplateFactory.getItemTemplate('Flow');
-  console.log(this.flowCategories);
+  self.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  self.currentMonth = null;
+  self.currentMonthIndex = null;
+  var templateFactory = TemplateFactory;
+  self.flowCategories = templateFactory.getItemTemplate("Flow");
 
+  console.log('flow categories', self.flowCategories);
 
-  this.enterMonthFlowData = function(month, i) {
+  self.enterMonthFlowData = function(month, i) {
     console.log('you have clicked', month);
     console.log(i);
-    this.currentMonth = month;
-    this.currentMonthIndex = i;
-    console.log(this.currentMonth);
+    self.currentMonth = month;
+    self.currentMonthIndex = i;
+    console.log(self.currentMonth);
   }
 
-  this.nextMonth = function() {
-    if(this.currentMonthIndex < 11) {
-    this.currentMonthIndex++;
+  self.nextMonth = function() {
+    if(self.currentMonthIndex < 11) {
+    self.currentMonthIndex++;
   } else {
-    this.currentMonthIndex = 0;
+    self.currentMonthIndex = 0;
   }
-    return this.currentMonth = this.months[this.currentMonthIndex];
+    return self.currentMonth = self.months[self.currentMonthIndex];
   }
 
-  this.prevMonth = function() {
-    if(this.currentMonthIndex > 0) {
-    this.currentMonthIndex--;
+  self.prevMonth = function() {
+    if(self.currentMonthIndex > 0) {
+    self.currentMonthIndex--;
   } else {
-    this.currentMonthIndex = 11;
+    self.currentMonthIndex = 11;
   }
-    return this.currentMonth = this.months[this.currentMonthIndex];
+    return self.currentMonth = self.months[this.currentMonthIndex];
   }
 }]);

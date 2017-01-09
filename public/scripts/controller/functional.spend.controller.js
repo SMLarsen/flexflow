a56737c9ac9ac8f6c0585696846b9428c2ea4832
Andy Wolff <andy.wolff@gmail.com>
2017-01-09 15:08:41 -0600

    fixing functional to basic

--- public/scripts/controller/functional.spend.controller.js
+++ public/scripts/controller/functional.spend.controller.js
@@ -1,15 +1,3 @@
 app.controller('FunctionalSpendController', ['$http', 'AuthFactory', 'TemplateFactory', function($http, AuthFactory, TemplateFactory) {
   console.log('Functional Spend controller started');
-
-// Add functional spending to DB
-  self.addFunctionalSpenging = function () {
-  		console.log('new article: ', self.newArticle);
-  		$http.post('/insertArticle', self.newArticle)
-  			.then(function (response) {
-  				console.log('POST finished. Get articles again.');
-  				getArticles();
-  			});
-  	};
-
-
 }]);

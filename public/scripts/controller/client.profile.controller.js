aec8687ffc50d881f279b6d37820faaa6b3acf97
Joseph Giwoyna <jgiwoyna@gmail.com>
2017-01-10 11:28:20 -0600

    added modal for client profile info

--- public/scripts/controller/client.profile.controller.js
+++ public/scripts/controller/client.profile.controller.js
@@ -1,8 +1,28 @@
 app.controller('ClientProfileController', ['$http', 'AuthFactory', 'TemplateFactory', function($http, AuthFactory, TemplateFactory) {
   console.log('Client Profile controller started');
 
+  var self = this;
+
   var templateFactory = TemplateFactory;
 
-  var self = this;
+  self.numPeople = 0;
+
+  self.clickOnePerson = function() {
+    self.numPeople = 1;
+  }
+
+  self.clickTwoPeople = function() {
+    self.numPeople = 2;
+  }
+
+  self.clickThreePeople = function() {
+    self.numPeople = 3;
+  }
+
+  self.clickFourPeople = function() {
+    self.numPeople = 4;
+  }
+
+
 
 }]);

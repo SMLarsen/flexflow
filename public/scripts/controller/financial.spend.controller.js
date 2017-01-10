8c1208966d9ca02aa608b8d445d49bd4bfc15e2b
Andrew Wolff <andy.wolff@gmail.com>
2017-01-10 14:15:17 -0600

    basic functional spend, starting modal

--- public/scripts/controller/financial.spend.controller.js
+++ public/scripts/controller/financial.spend.controller.js
@@ -1,3 +1,10 @@
-app.controller('FinancialSpendController', ['$http', 'AuthFactory', 'TemplateFactory', function($http, AuthFactory, TemplateFactory) {
-  console.log('Financial Spend controller started');
+app.controller('FinancialSpendController', ['$http', 'AuthFactory', 'TemplateFactory', function ($http, AuthFactory, TemplateFactory) {
+	console.log('Financial Spend controller started');
+
+	var self = this;
+	var templateFactory = TemplateFactory;
+
+	self.itemArray = templateFactory.getItemTemplate("Financial");
+	console.log("financial itemArray ", self.itemArray);
+
 }]);
--- public/scripts/controller/functional.spend.controller.js
+++ public/scripts/controller/functional.spend.controller.js
@@ -1,15 +1,9 @@
-app.controller('FunctionalSpendController', ['$http', 'AuthFactory', 'TemplateFactory', function($http, AuthFactory, TemplateFactory) {
-  console.log('Functional Spend controller started');
+app.controller('FunctionalSpendController', ['$http', 'AuthFactory', 'TemplateFactory', function ($http, AuthFactory, TemplateFactory) {
+	console.log('Functional Spend controller started');
 
-// Add functional spending to DB
-  self.addFunctionalSpenging = function () {
-  		console.log('new article: ', self.newArticle);
-  		$http.post('/insertArticle', self.newArticle)
-  			.then(function (response) {
-  				console.log('POST finished. Get articles again.');
-  				getArticles();
-  			});
-  	};
+	var self = this;
+	var templateFactory = TemplateFactory;
 
-
+	self.itemArray = templateFactory.getItemTemplate("Functional");
+	console.log("functional itemArray ", self.itemArray);
 }]);

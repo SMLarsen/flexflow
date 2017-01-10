e09a03a9e245d9ae63a19864962feca5b5f0cc56
Andrew Wolff <andy.wolff@gmail.com>
2017-01-10 16:30:22 -0600

    ng-show needs a little more work, modal mostly built, financial partially built out

--- public/scripts/controller/functional.spend.controller.js
+++ public/scripts/controller/functional.spend.controller.js
@@ -2,8 +2,28 @@ app.controller('FunctionalSpendController', ['$http', 'AuthFactory', 'TemplateFa
 	console.log('Functional Spend controller started');
 
 	var self = this;
+	self.newFunctionalBudget = {};
 	var templateFactory = TemplateFactory;
-
 	self.itemArray = templateFactory.getItemTemplate("Functional");
 	console.log("functional itemArray ", self.itemArray);
+
+	// adding false value to itemArray
+	removeActiveToggles();
+
+	// toggles activeitem value for each item
+	self.toggleActive = function (item) {
+		if (item.activeitem === false) {
+			item.activeitem = true;
+		} else {
+			item.activeitem = false;
+		}
+	}
+
+	// removes all active values in individual functional items
+	function removeActiveToggles() {
+		for (var i = 0; i < self.itemArray.length; i++) {
+			var item = self.itemArray[i];
+			item.activeItem = false;
+		}
+	}
 }]);

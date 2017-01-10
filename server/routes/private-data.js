6e6e74cc6b14587906adb3e38518635dc2cb2ccb
Andrew Wolff <andy.wolff@gmail.com>
2017-01-10 16:34:08 -0600

    ng-show needs a little more work, modal mostly built, financial partially built out

--- server/routes/private-data.js
+++ server/routes/private-data.js
@@ -51,55 +51,54 @@ router.get("/privateData", function (req, res) {
 			}
 		});
 	});
-router.get("/privateData", function(req, res) {
-    pg.connect(connectionString, function(err, client, done) {
-        var userEmail = req.decodedToken.email;
-        console.log(userEmail);
-        /* getting user by email */
-        client.query('SELECT email, clearance_level FROM users WHERE email=$1', [userEmail], function(err, result) {
-            done();
-            if (err) {
-                console.log('Error COMPLETING clearance_level query task', err);
-                res.sendStatus(500);
-            } else {
-                pg.connect(connectionString, function(err, client, done) {
-                    /* if user does not exist, add to user table */
-                    if (result.rowCount === 0) {
-                        client.query(
-                            'INSERT INTO users (email) VALUES ($1)', [userEmail],
-                            function(err, result) {
-                                done();
-                                if (err) {
-                                    console.log('error on user Insert', err);
-                                    res.sendStatus(500);
-                                } else {
-                                    /* Retrieve new user to get id */
-                                    client.query('SELECT email, clearance_level FROM users WHERE email=$1', [userEmail], function(err, result) {
-                                        done();
-                                        if (err) {
-                                            console.log('Error Selecting new user', err);
-                                            res.sendStatus(500);
-                                        } else {
-                                            var currentUser = result.rows[0];
-                                            currentUser.newUser = true;
-                                            console.log('new User:', currentUser);
-                                            res.send(currentUser);
-                                        }
-                                    });
-                                }
-                            });
-                    } else {
-                        var currentUser = result.rows[0];
-                        currentUser.newUser = false;
-                        console.log('currentUser:', currentUser);
-                        res.send(currentUser);
-                    }
-                    done();
-                });
-            }
-        });
-    });
->>>>>>> eeacb89104b1ca0f4eba1f527019872a784d059f
+	router.get("/privateData", function (req, res) {
+		pg.connect(connectionString, function (err, client, done) {
+			var userEmail = req.decodedToken.email;
+			console.log(userEmail);
+			/* getting user by email */
+			client.query('SELECT email, clearance_level FROM users WHERE email=$1', [userEmail], function (err, result) {
+				done();
+				if (err) {
+					console.log('Error COMPLETING clearance_level query task', err);
+					res.sendStatus(500);
+				} else {
+					pg.connect(connectionString, function (err, client, done) {
+						/* if user does not exist, add to user table */
+						if (result.rowCount === 0) {
+							client.query(
+								'INSERT INTO users (email) VALUES ($1)', [userEmail],
+								function (err, result) {
+									done();
+									if (err) {
+										console.log('error on user Insert', err);
+										res.sendStatus(500);
+									} else {
+										/* Retrieve new user to get id */
+										client.query('SELECT email, clearance_level FROM users WHERE email=$1', [userEmail], function (err, result) {
+											done();
+											if (err) {
+												console.log('Error Selecting new user', err);
+												res.sendStatus(500);
+											} else {
+												var currentUser = result.rows[0];
+												currentUser.newUser = true;
+												console.log('new User:', currentUser);
+												res.send(currentUser);
+											}
+										});
+									}
+								});
+						} else {
+							var currentUser = result.rows[0];
+							currentUser.newUser = false;
+							console.log('currentUser:', currentUser);
+							res.send(currentUser);
+						}
+						done();
+					});
+				}
+			});
+		});
+	});
 });
-
 module.exports = router;

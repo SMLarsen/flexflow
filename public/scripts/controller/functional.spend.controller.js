1b6c3b4230381ccf318824e453b7366bdb8115e5
Andrew Wolff <andy.wolff@gmail.com>
2017-01-11 11:28:07 -0600

    working on pulling data

--- public/scripts/controller/functional.spend.controller.js
+++ public/scripts/controller/functional.spend.controller.js
@@ -1,30 +1,33 @@
-
-app.controller('FunctionalSpendController', ['$http', 'AuthFactory', 'TemplateFactory', function ($http, AuthFactory, TemplateFactory) {
+app.controller('FunctionalSpendController', ['$http', 'AuthFactory', 'TemplateFactory', 'BudgetFactory', function ($http, AuthFactory, TemplateFactory, BudgetFactory) {
 	console.log('Functional Spend controller started');
 
 	var self = this;
-	self.newFunctionalBudget = {};
-	var templateFactory = TemplateFactory;
-	self.itemArray = templateFactory.getItemTemplate("Functional");
-	console.log("functional itemArray ", self.itemArray);
+	// self.newFunctionalBudget = {};
+	// var templateFactory = TemplateFactory;
+	// self.itemArray = templateFactory.getItemTemplate("Functional");
+	// console.log("functional itemArray ", self.itemArray);
+	//
+	// // adding false value to itemArray
+	// removeActiveToggles();
+	//
+	// // toggles activeitem value for each item
+	// self.toggleActive = function (item) {
+	// 	if (item.activeitem === false) {
+	// 		item.activeitem = true;
+	// 	} else {
+	// 		item.activeitem = false;
+	// 	}
+	// };
+	//
+	// // removes all active values in individual functional items
+	// function removeActiveToggles() {
+	// 	for (var i = 0; i < self.itemArray.length; i++) {
+	// 		var item = self.itemArray[i];
+	// 		item.activeItem = false;
+	// 	}
+	// }
 
-	// adding false value to itemArray
-	removeActiveToggles();
+	var budgetfactory = BudgetFactory;
+	console.log('budgetfactory ', budgetfactory.getFunctionalItems());
 
-	// toggles activeitem value for each item
-	self.toggleActive = function (item) {
-		if (item.activeitem === false) {
-			item.activeitem = true;
-		} else {
-			item.activeitem = false;
-		}
-	};
-
-	// removes all active values in individual functional items
-	function removeActiveToggles() {
-		for (var i = 0; i < self.itemArray.length; i++) {
-			var item = self.itemArray[i];
-			item.activeItem = false;
-		}
-	}
 }]);

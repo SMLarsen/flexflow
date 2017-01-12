84548587045f694c5daedeb248f460398f4524c0
Joseph Giwoyna <jgiwoyna@gmail.com>
2017-01-12 14:31:27 -0600

    deleted congrats controller and template and replaced with modal

--- public/scripts/controller/congrats.controller.js
+++
@@ -1,8 +0,0 @@
-app.controller('CongratsController', ['$http', 'AuthFactory', 'TemplateFactory', function($http, AuthFactory, TemplateFactory) {
-  console.log('Congrats controller started');
-
-  var self = this;
-
-  var templateFactory = TemplateFactory;
-  
-}]);

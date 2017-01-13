37de5bfad3a3672bf83ac30c5dfa66d3240d6a00
Joseph Giwoyna <jgiwoyna@gmail.com>
2017-01-13 09:29:39 -0600

    added comments and removed unused dependencies in controllers

--- public/scripts/controller/savings.goals.controller.js
+++ public/scripts/controller/savings.goals.controller.js
@@ -1,27 +1,23 @@
-app.controller('SavingsGoalsController', ['$http', 'AuthFactory', 'TemplateFactory', 'BudgetFactory', function($http, AuthFactory, TemplateFactory, BudgetFactory) {
+app.controller('SavingsGoalsController', ['BudgetFactory', function(BudgetFactory) {
   console.log('Savings Goals controller started');
 
   var self = this;
-  var templateFactory = TemplateFactory;
+
   var budgetFactory = BudgetFactory;
+
   self.budget = {};
   self.savings = undefined;
   self.monthSavings = undefined;
 
-
-
-
-
   getSavings();
+
+  // Function for getting savings from DB and calculating yearly and monthly
   function getSavings() {
     budgetFactory.getBudget().then(function(response){
       self.budget = response;
       self.savings = self.budget.annual_salary * .20;
       self.monthSavings = self.savings / 12;
     });
-  }
+  }; // End: getSavings
 
-
-
-
-}]);
+}]); // END: SavingsGoalsController

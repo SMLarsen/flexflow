14f17990e87f06312420f18484a32e7c5d9cedd3
Joseph Giwoyna <jgiwoyna@gmail.com>
2017-01-16 13:52:43 -0600

    fixed toggles, removed unused code

--- public/scripts/controller/financial.spend.controller.js
+++ public/scripts/controller/financial.spend.controller.js
@@ -2,17 +2,18 @@ app.controller('FinancialSpendController', ['$http', 'AuthFactory', 'TemplateFac
 	console.log('Financial Spend controller started');
 
 	var self = this;
-	self.newFinancialBudget = {};
+
 	var budgetFactory = BudgetFactory;
 
 	budgetFactory.getFinancialItems().then(function (result) {
 		self.itemArray = result;
-		removeActiveToggles();
+		setToggles();
+		resetZeroValues();
 		console.log("financial self.itemArray ", self.itemArray);
 	});
 
 	// toggles activeitem value for each item
-	self.toggleActive = function (item) {
+	self.toggleActive = function(item) {
 		if (item.activeItem === false) {
 			item.activeItem = true;
 		} else {
@@ -20,13 +21,26 @@ app.controller('FinancialSpendController', ['$http', 'AuthFactory', 'TemplateFac
 		}
 	};
 
-	// removes all active values in individual functional items
-	function removeActiveToggles() {
+	// removes all active values in individual flow categories
+  function setToggles(){
+    for (var i = 0; i < self.itemArray.length; i++) {
+      var category = self.itemArray[i];
+      if(category.item_amount === undefined || category.item_amount === 0 || category.item_amount === null) {
+        category.activeItem  = false;
+      } else {
+        category.activeItem = true;
+      }
+    }
+  } // end setToggles
+
+	// function to ensure zero values show up as placeholder in inputs
+	function resetZeroValues() {
 		for (var i = 0; i < self.itemArray.length; i++) {
-			var item = self.itemArray[i];
-			item.activeItem = false;
+			if(self.itemArray[i].item_amount == 0) {
+				self.itemArray[i].item_amount = null;
+			}
 		}
-	}
+	} // end resetZeroValues
 
 	self.getFinancialItems = function () {
 		console.log("getFinancialItems is clicked");
@@ -38,4 +52,5 @@ app.controller('FinancialSpendController', ['$http', 'AuthFactory', 'TemplateFac
 		budgetFactory.updateFinancialItems(self.itemArray);
 		window.location = '/#/additionalinfo';
 	};
+
 }]);
--- public/scripts/controller/functional.spend.controller.js
+++ public/scripts/controller/functional.spend.controller.js
@@ -7,9 +7,10 @@ app.controller('FunctionalSpendController', ['$http', 'AuthFactory', 'TemplateFa
 
 	self.newCategory = {};
 
-	budgetFactory.getFunctionalItems().then(function (result) {
+	budgetFactory.getFunctionalItems().then(function(result) {
 		self.itemArray = result;
-		removeActiveToggles();
+		setToggles();
+		resetZeroValues();
 		console.log("functional self.itemArray ", self.itemArray);
 	});
 
@@ -22,33 +23,47 @@ app.controller('FunctionalSpendController', ['$http', 'AuthFactory', 'TemplateFa
 		}
 	};
 
-	// removes all active values in individual functional items
-	function removeActiveToggles() {
-		for (var i = 0; i < self.itemArray.length; i++) {
-			var item = self.itemArray[i];
-			item.activeItem = false;
-		}
-	}
+	// removes all active values in individual flow categories
+  function setToggles(){
+    for (var i = 0; i < self.itemArray.length; i++) {
+      var category = self.itemArray[i];
+      if(category.item_amount === undefined || category.item_amount === 0 || category.item_amount === null) {
+        category.activeItem  = false;
+      } else {
+        category.activeItem = true;
+      }
+    }
+  } // end setToggles
 
-	self.getFunctionalItems = function () {
+	// function to ensure zero values show up as placeholder in inputs
+	function resetZeroValues() {
+		for (var i = 0; i < self.itemArray.length; i++) {
+			if(self.itemArray[i].item_amount == 0) {
+				self.itemArray[i].item_amount = null;
+			}
+		}
+	} // end resetZeroValues
+
+	self.getFunctionalItems = function() {
 		console.log("getFunctionalItems is clicked");
 		budgetFactory.getFunctionalItems(self.itemArray);
 	};
 
-	self.updateFunctionalItems = function () {
+	self.updateFunctionalItems = function() {
 		console.log("update functional clicked");
 		budgetFactory.updateFunctionalItems(self.itemArray);
-		// window.location = '/#/financialspend';
+		window.location = '/#/financialspend';
 	};
 
-	self.addFunctionalItem = function(){
+	self.addFunctionalItem = function() {
 		if(self.newCategory.item_name === null || self.newCategory.item_amount === null) {
 			alert('Please enter an additional category before submitting.');
 		} else {
 			console.log(self.newCategory);
+			self.newCategory.activeItem = true;
+			self.newCategory.item_sort_sequence = self.itemArray.length + 2;
+			self.newCategory.budget_template_category_id = 4;
 			self.itemArray.push(self.newCategory);
-			self.updateFunctionalItems();
-			self.itemArray = [];
 		}
 	};
 

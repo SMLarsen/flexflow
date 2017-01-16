492c5e2da30b1df4c95ef32380e446a87ea7983a
Steve Larsen <stevelarsen01@gmail.com>
2017-01-16 17:05:27 -0600

    retrieving all items from db

--- server/routes/pdf.js
+++ server/routes/pdf.js
@@ -1,15 +1,62 @@
 var express = require('express');
 var router = express.Router();
-var doc = require('../modules/pdf-report');
+var pg = require('pg');
+// var connectionString = require('../modules/database-config');
+var config = require('../modules/pg-config');
 
-router.get("/", function(req, res) {
-    console.log('***********  Starting PDF route  ***********');
-
-    doc.makeClientPDF();
-    console.log('***********  Ending PDF route  ***********');
-    doc.done();
-    next();
-
+var pool = new pg.Pool({
+    database: config.database
 });
+
+// Route: GET flow monthly totals for a budget
+router.get("/", function(req, res, next) {
+    pool.connect()
+        .then(function(client) {
+            var queryString = 'SELECT item_year, item_month, item_name, item_amount ';
+            queryString += 'FROM budget_flow_item ';
+            queryString += 'WHERE budget_id = $1 ';
+            queryString += 'ORDER BY item_year, item_month, item_sort_sequence';
+            // console.log('queryString:', queryString);
+            client.query(queryString, [req.budgetID], function(err, result) {
+                if (err) {
+                    console.log('Error getting flow items monthly totals', err);
+                    client.release();
+                    next();
+                } else {
+                  console.log('Flow:', result.rows);
+                    console.log('Flow items monthly totals retrieved');
+                    client.release();
+                    next();
+                }
+            });
+        });
+});
+
+
+// Route: GET non-flow totals for a budget
+router.get("/", function(req, res, next) {
+    pool.connect()
+        .then(function(client) {
+            var queryString = 'SELECT category_name, item_name, item_amount ';
+            queryString += 'FROM budget_item, budget_template_category ';
+            queryString += 'WHERE budget_template_category.id = budget_template_category_id ';
+            queryString += 'AND budget_id = $1 ';
+            queryString += 'ORDER BY budget_template_category_id, item_sort_sequence';
+            // console.log('queryString:', queryString);
+            client.query(queryString, [req.budgetID], function(err, result) {
+                if (err) {
+                    console.log('Error getting non-flow items monthly totals', err);
+                    client.release();
+                    next();
+                } else {
+                    console.log('Non-flow:', result.rows);
+                    console.log('Non-Flow items monthly totals retrieved');
+                    client.release();
+                    next();
+                }
+            });
+        });
+});
+
 
 module.exports = router;

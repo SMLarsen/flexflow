2b6656702ebd3c51d3000ee28cde86696dac131f
Steve Larsen <stevelarsen01@gmail.com>
2017-01-16 18:31:12 -0600

    build pdf data - flow and flex

--- server/routes/pdf.js
+++ server/routes/pdf.js
@@ -8,6 +8,9 @@ var pool = new pg.Pool({
     database: config.database
 });
 
+var pdfData = {};
+
+
 // Route: GET flow monthly totals for a budget
 router.get("/", function(req, res, next) {
     pool.connect()
@@ -23,8 +26,8 @@ router.get("/", function(req, res, next) {
                     client.release();
                     next();
                 } else {
-                  console.log('Flow:', result.rows);
                     console.log('Flow items monthly totals retrieved');
+                    formatFlowItems(result.rows);
                     client.release();
                     next();
                 }
@@ -32,6 +35,28 @@ router.get("/", function(req, res, next) {
         });
 });
 
+function formatFlowItems(itemMonthArray) {
+    console.log('formatFlowItems', itemMonthArray.length);
+    var itemMonthColumn = [];
+    var itemYear = [];
+    var monthTotal = 0;
+    var yearTotal = 0;
+    for (var i = 0; i < itemMonthArray.length; i++) {
+        monthTotal += itemMonthArray[i].item_amount;
+        itemMonthColumn.push(itemMonthArray[i]);
+        if ((i + 1) % (itemMonthArray.length / 12) === 0) {
+            itemMonthColumn.push({
+                item_name: 'Monthly Total',
+                item_amount: monthTotal
+            });
+            itemYear.push(itemMonthColumn);
+            itemMonthColumn = [];
+            monthTotal = 0;
+        }
+    }
+    pdfData.Flow = itemYear;
+    // console.log(pdfData.flow);
+}
 
 // Route: GET non-flow totals for a budget
 router.get("/", function(req, res, next) {
@@ -49,7 +74,7 @@ router.get("/", function(req, res, next) {
                     client.release();
                     next();
                 } else {
-                    console.log('Non-flow:', result.rows);
+                    formatNonFlowItems(result.rows);
                     console.log('Non-Flow items monthly totals retrieved');
                     client.release();
                     next();
@@ -58,5 +83,41 @@ router.get("/", function(req, res, next) {
         });
 });
 
+function formatNonFlowItems(itemMonthArray) {
+    console.log('formatNonFlowItems', itemMonthArray.length);
+    var itemMonthColumn = [];
+    var itemYear = [];
+    var monthTotal = 0;
+    var yearTotal = 0;
+    var currentCategory = itemMonthArray[0].category_name;
+    for (var i = 0; i < itemMonthArray.length; i++) {
+        monthTotal += itemMonthArray[i].item_amount;
+        itemMonthColumn.push(itemMonthArray[i]);
+        if ((i + 1) < itemMonthArray.length) {
+          console.log(12341234);
+            if (currentCategory !== itemMonthArray[i + 1].category_name) {
+                itemMonthColumn.push({
+                    item_name: 'Monthly Total',
+                    item_amount: monthTotal
+                });
+                for (var mm = 1; mm <= 12; mm++) {
+                    itemYear.push(itemMonthColumn);
+                }
+                pdfData[currentCategory] = itemYear;
+                itemYear = [];
+                itemMonthColumn = [];
+                monthTotal = 0;
+                currentCategory = itemMonthArray[i + 1].category_name;
+            }
+        } else {
+          currentCategory = itemMonthArray[i].category_name;
+            pdfData[currentCategory] = itemYear;
+            itemYear = [];
+            itemMonthColumn = [];
+            monthTotal = 0;
+        }
+    }
+    console.log(pdfData);
+}
 
 module.exports = router;

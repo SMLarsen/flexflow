10369222809c2c7c534e444415b96d3f8a68fd50
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-01-17 23:37:19 -0600

    work on edit comment

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -10,10 +10,10 @@ var pool = new pg.Pool({
 });
 
 router.get('/', function(req, res) {
-    console.log("here in comment ", pool);
+    // /console.log("here in comment ", pool);
     pool.connect()
         .then(function(client) {
-          var queryString = 'SELECT budget_comment, id, created_at FROM budget_comment WHERE budget_id = $1';
+            var queryString = 'SELECT budget_comment, id, created_at FROM budget_comment WHERE budget_id = $1';
             client.query(queryString, [req.budgetID])
                 .then(function(result) {
                     client.release();
@@ -27,23 +27,53 @@ router.get('/', function(req, res) {
         }); // end first then
 }); // end pool.connect
 
-router.post('/', function(req, res){
-  console.log("here in post comment");
-  pool.connect()
-    .then(function(client){
-      // make query
-      client.query('INSERT INTO budget_comment (budget_id, budget_comment) VALUES($1, $2)',
-      [req.budgetID, req.body.budget_comment])
-        .then(function(result){
-          client.release();
-          res.sendStatus(201);
-        }).catch(function(err){
-          // error
-          client.release();
-          console.log('Error on POST comment', err);
-          res.sendStatus(500);
+router.post('/', function(req, res) {
+    console.log("here in post comment");
+    pool.connect()
+        .then(function(client) {
+            // make query
+            client.query('INSERT INTO budget_comment (budget_id, budget_comment) VALUES($1, $2)',
+            [req.budgetID, req.body.budget_comment])
+                .then(function(result) {
+                    client.release();
+                    res.sendStatus(201);
+                }).catch(function(err) {
+                    // error
+                    client.release();
+                    console.log('Error on POST comment', err);
+                    res.sendStatus(500);
+                });
+        }); // end first then
+});
+
+router.put('/:id', function(req, res) {
+    console.log("here in edit comment");
+    // Grab data from the URL parameters
+    var commentID = req.params.id;
+
+    // the data to send to edit
+    var data = {
+        budget_comment: req.body.budget_comment,
+        created_at: req.body.created_at
+    };
+
+    pool.connect()
+        .then(function(client) {
+          // make query
+          client.query('UPDATE budget_comment SET budget_comment=($1), created_at=($2) WHERE id=($3)',
+          [data.budget_comment, data.created_at, commentID])
+            .then(function(result){
+              client.release();
+              res.sendStatus(201);
+            }).catch(function(err){
+              // error
+              client.release();
+              console.log('Error on EDIT comment', err);
+              res.sendStatus(500);
+            });
         });
-    }); // end first then
+
+
 });
 
 

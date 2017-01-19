0d2b20c819f3c99b5cfc24a41a2c739f536bf492
Joseph Giwoyna <jgiwoyna@gmail.com>
2017-01-19 16:23:33 -0600

    changed favicon to becoming financial and fixed bugs with csv

--- server/routes/csv.js
+++ server/routes/csv.js
@@ -110,13 +110,11 @@ router.post("/", function(req, res, next) {
                 if (err) {
                     console.log('Error getting comment for reporting', err);
                     client.release();
-                    res.sendStatus(500);
                     next();
                 } else {
                     // console.log('Reporting comment retrieved');
                     csvData.comment = result.rows;
                     client.release();
-                    res.sendStatus(201);
                     next();
                 }
             });
@@ -215,14 +213,14 @@ router.post("/", function(req, res) {
     // console.log("im here in send mail");
     // var name =
     //csv.router();
-    var filePath = path.join(__dirname, '../csv/flexflow-' + req.budgetID + '.csv');
+    var filePath = path.join(__dirname, './flexflow-' + req.budgetID + '.csv');
 
-    var htmlObject = '<p>You have a submission with the folowing details...' + '<br>' +
+    var htmlObject = '<p>You have a submission with the following details...' + '<br>' +
         "Name: " + req.body.displayName + '<br>' +
         "Email: " + req.body.email + '<br>' +
-        "FlowTotal: " + req.body.flowTotal + '<br>' +
-        "FlexTotal: " + req.body.flexTotal + '<br>' +
-        "FunctionalTotal: " + req.body.functionalTotal + '<br>' +
+        "Flow Total: $" + req.body.flowTotal + '<br>' +
+        "Flex Total: $" + req.body.flexTotal + '<br>' +
+        "Functional Total: $" + req.body.functionalTotal + '<br>' +
         "Financial Total: $" + req.body.financialTotal + '<br>' +
         "Monthly Take Home: $" + req.body.takeHomeCash + '<br>' +
         "Net Total: $" + req.body.netTotal + '</p>';
@@ -278,7 +276,7 @@ var json2csvLastCallback = function(err, csv) {
     if (err) throw err;
     csvContent += csv;
     var fileName = 'flexflow-' + budgetID + '.csv';
-    fs.writeFile("./server/csv/" + fileName, csvContent, function(err) {
+    fs.writeFile("./server/routes/" + fileName, csvContent, function(err) {
         if (err) {
             console.log(err);
         }

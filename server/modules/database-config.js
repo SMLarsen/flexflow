6f2356435f846e6c2d40669a860b0a2ad6694ca2
Andrew Wolff <andy.wolff@gmail.com>
2017-01-21 11:16:22 -0600

    configuring db

--- server/modules/database-config.js
+++ server/modules/database-config.js
@@ -1,1 +1,18 @@
-module.exports = 'postgres://localhost:5432/flexflow';
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+
+var herokuDB = 
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT * FROM budget;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+
+module.exports = herokuDB;

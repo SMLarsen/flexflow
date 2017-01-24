aa0c86b93f9354ef87f2723dfec205355b441a26
Andrew Wolff <andy.wolff@gmail.com>
2017-01-24 13:25:11 -0600

    db config

--- server/modules/database-config.js
+++ server/modules/database-config.js
@@ -1,18 +1,15 @@
 var pg = require('pg');
 
 pg.defaults.ssl = true;
-
-var herokuDB = 
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
 
   client
-    .query('SELECT * FROM budget;')
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
     .on('row', function(row) {
       console.log(JSON.stringify(row));
     });
 });
 
-
-module.exports = herokuDB;
+module.exports = process.env.DATABASE_URL;
--- server/modules/pg-config.js
+++ server/modules/pg-config.js
@@ -1,5 +1,11 @@
-var config = {
-  database: 'flexflow'
-};
+var config = function () {
+// If we are running on Heroku, use the remote database (with SSL)
+if(process.env.DATABASE_URL != undefined) {
+    connectionString = process.env.DATABASE_URL + "?ssl=true";
+} else {
+    // running locally, use our local database instead
+  database: 'flexflow';
 
+}
+};
 module.exports = config;

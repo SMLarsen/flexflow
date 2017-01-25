b472570c988703ea1c621bd668f53bd1b1ab2a67
Andrew Wolff <andy.wolff@gmail.com>
2017-01-25 13:41:41 -0600

    heroku db

--- server/modules/pg-config.js
+++ server/modules/pg-config.js
@@ -4,7 +4,7 @@ var config = {
   password: process.env.DATA_BASE_PASSWORD, //env var: PGPASSWORD
   host: process.env.DATA_BASE_HOST, // Server hosting the postgres database
   ssl: true,
-  port: 5432,
+  // port: 5432,
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };

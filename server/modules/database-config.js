9e48f8cb8c093c4471a3ff0aecbfbbff4c66c654
Andrew Wolff <andy.wolff@gmail.com>
2017-01-25 13:10:27 -0600

    heroku db

--- server/modules/database-config.js
+++ server/modules/database-config.js
@@ -1,1 +1,1 @@
-module.exports = process.env.DATABASE_URL;
+module.exports = 'postgres://localhost:5432/flexflow';
--- server/modules/pg-config.js
+++ server/modules/pg-config.js
@@ -1,5 +1,12 @@
 var config = {
-  database: 'flexflow'
+  user: process.env.DATA_BASE_USER,
+  database: process.env.DATA_BASE,
+  password: process.env.DATA_BASE_PASSWORD, //env var: PGPASSWORD
+  host: 'localhost', // Server hosting the postgres database
+  ssl: true,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
 
 module.exports = config;
+

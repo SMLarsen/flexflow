e600af74a558b902e120963b6f7df868d9511d8f
Andrew Wolff <andy.wolff@gmail.com>
2017-01-25 13:33:01 -0600

    heroku db

--- server/modules/pg-config.js
+++ server/modules/pg-config.js
@@ -2,7 +2,7 @@ var config = {
   user: process.env.DATA_BASE_USER,
   database: process.env.DATA_BASE,
   password: process.env.DATA_BASE_PASSWORD, //env var: PGPASSWORD
-  host: 'localhost', // Server hosting the postgres database
+  host: process.env.DATA_BASE_HOST, // Server hosting the postgres database
   ssl: true,
   port: 5432,
   max: 10, // max number of clients in the pool

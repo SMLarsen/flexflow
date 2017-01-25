db28e74a0ff53423517434d2f0cb19b3df50451c
Andrew Wolff <andy.wolff@gmail.com>
2017-01-25 13:46:23 -0600

    heroku db

--- server/modules/pg-config.js
+++ server/modules/pg-config.js
@@ -1,13 +1,25 @@
 var config = {
-  user: process.env.DATA_BASE_USER,
-  database: process.env.DATA_BASE,
-  password: process.env.DATA_BASE_PASSWORD, //env var: PGPASSWORD
-  host: process.env.DATA_BASE_HOST, // Server hosting the postgres database
+  user: "ggsnfqqetabdkl",
+  database: "d993oqov983da6",
+  password: "513e528808035aadbce8b45677d6fb4bfa5bd65387a15b1ac398c3171ea6b6a9", 
+  host: "ec2-54-225-66-44.compute-1.amazonaws.com", // Server hosting the postgres database
   ssl: true,
-  // port: 5432,
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
 
 module.exports = config;
 
+
+// var config = {
+//   user: process.env.DATA_BASE_USER,
+//   database: process.env.DATA_BASE,
+//   password: process.env.DATA_BASE_PASSWORD, //env var: PGPASSWORD
+//   host: process.env.DATA_BASE_HOST, // Server hosting the postgres database
+//   ssl: true,
+//   // port: 5432,
+//   max: 10, // max number of clients in the pool
+//   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+// };
+
+// module.exports = config;

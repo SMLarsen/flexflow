2bcea4f16aefc19c8ca0bba3d43438df1a657c94
Steve Larsen <stevelarsen01@gmail.com>
2017-01-25 11:03:16 -0600

    remove extraneous console logs

--- public/scripts/controller/functional.spend.controller.js
+++ public/scripts/controller/functional.spend.controller.js
@@ -1,135 +1,131 @@
-app.controller('FunctionalSpendController', ['BudgetFactory', function (BudgetFactory) {
-	console.log('Functional Spend controller started');
+app.controller('FunctionalSpendController', ['BudgetFactory', function(BudgetFactory) {
+    // console.log('Functional Spend controller started');
 
-	var self = this;
+    var self = this;
 
-	var budgetFactory = BudgetFactory;
+    var budgetFactory = BudgetFactory;
 
-	self.navActive = false;
+    self.navActive = false;
 
-	self.activateMobileNav = function() {
-		if(self.navActive === false){
-			self.navActive = true;
-		} else {
-			self.navActive = false;
-		}
-	};
+    self.activateMobileNav = function() {
+        if (self.navActive === false) {
+            self.navActive = true;
+        } else {
+            self.navActive = false;
+        }
+    };
 
-	budgetFactory.getBudget().then(function(results){
-		self.budget = results;
-		self.budgetStatus = self.budget.budget_status;
-    switch (self.budgetStatus) {
-      case "Finished":
-      self.budgetStatusIndex = 6;
-      break;
-      case "Comments":
-      self.budgetStatusIndex = 6;
-      break;
-      case "Financial":
-      self.budgetStatusIndex = 5;
-      break;
-      case "Functional":
-      self.budgetStatusIndex = 4;
-      break;
-      case "Flow":
-      self.budgetStatusIndex = 3;
-      break;
-      case "Flex":
-      self.budgetStatusIndex = 2;
-      break;
-      case "Profile":
-      self.budgetStatusIndex = 1;
-      break;
-      default:
-      self.budgetStatusIndex = 0;
+    budgetFactory.getBudget().then(function(results) {
+        self.budget = results;
+        self.budgetStatus = self.budget.budget_status;
+        switch (self.budgetStatus) {
+            case "Finished":
+                self.budgetStatusIndex = 6;
+                break;
+            case "Comments":
+                self.budgetStatusIndex = 6;
+                break;
+            case "Financial":
+                self.budgetStatusIndex = 5;
+                break;
+            case "Functional":
+                self.budgetStatusIndex = 4;
+                break;
+            case "Flow":
+                self.budgetStatusIndex = 3;
+                break;
+            case "Flex":
+                self.budgetStatusIndex = 2;
+                break;
+            case "Profile":
+                self.budgetStatusIndex = 1;
+                break;
+            default:
+                self.budgetStatusIndex = 0;
+        }
+    });
+
+    budgetFactory.getFunctionalItems().then(function(result) {
+        self.itemArray = result;
+        setToggles();
+        resetZeroValues();
+    });
+
+    // toggles activeItem value for each item
+    self.toggleActive = function(item) {
+        if (item.activeItem === false) {
+            item.activeItem = true;
+        } else {
+            item.activeItem = false;
+        }
+    };
+
+    // removes all active values in individual flow categories
+    function setToggles() {
+        for (var i = 0; i < self.itemArray.length; i++) {
+            var category = self.itemArray[i];
+            if (category.item_amount === undefined || category.item_amount === 0 || category.item_amount === null) {
+                category.activeItem = false;
+            } else {
+                category.activeItem = true;
+            }
+        }
+    } // end setToggles
+
+    // function to ensure zero values show up as placeholder in inputs
+    function resetZeroValues() {
+        for (var i = 0; i < self.itemArray.length; i++) {
+            if (self.itemArray[i].item_amount === 0) {
+                self.itemArray[i].item_amount = null;
+            }
+        }
+    } // end resetZeroValues
+
+    self.getFunctionalItems = function() {
+        budgetFactory.getFunctionalItems(self.itemArray);
+    };
+
+    self.updateFunctionalItems = function() {
+        setInactiveValuesToZero();
+        budgetFactory.updateFunctionalItems(self.itemArray).then(function(result) {
+            switch (self.budget.budget_status) {
+                case "Finished":
+                    budgetFactory.updateBudgetStatus("Finished");
+                    break;
+                case "Comments":
+                    budgetFactory.updateBudgetStatus("Comments");
+                    break;
+                case "Financial":
+                    budgetFactory.updateBudgetStatus("Financial");
+                    break;
+                case "Functional":
+                    budgetFactory.updateBudgetStatus("Functional");
+                    break;
+                default:
+                    budgetFactory.updateBudgetStatus("Functional");
+            }
+        });
+    };
+
+    self.addFunctionalItem = function() {
+        if (self.newCategory.item_name === null || self.newCategory.item_amount === null) {
+            alert('Please enter an additional category before submitting.');
+        } else {
+            self.newCategory.activeItem = true;
+            self.newCategory.item_img_src = 'additional.svg';
+            self.newCategory.item_sort_sequence = self.itemArray.length + 2;
+            self.newCategory.budget_template_category_id = 4;
+            self.itemArray.push(self.newCategory);
+            self.newCategory = {};
+        }
+    };
+
+    function setInactiveValuesToZero() {
+        for (i = 0; i < self.itemArray.length; i++) {
+            if (self.itemArray[i].activeItem === false) {
+                self.itemArray[i].item_amount = 0;
+            }
+        }
     }
-	});
-
-	budgetFactory.getFunctionalItems().then(function(result) {
-		self.itemArray = result;
-		setToggles();
-		resetZeroValues();
-		console.log("functional self.itemArray ", self.itemArray);
-	});
-
-	// toggles activeItem value for each item
-	self.toggleActive = function(item) {
-		if (item.activeItem === false) {
-			item.activeItem = true;
-		} else {
-			item.activeItem = false;
-		}
-	};
-
-	// removes all active values in individual flow categories
-  function setToggles() {
-    for (var i = 0; i < self.itemArray.length; i++) {
-      var category = self.itemArray[i];
-      if(category.item_amount === undefined || category.item_amount === 0 || category.item_amount === null) {
-        category.activeItem  = false;
-      } else {
-        category.activeItem = true;
-      }
-    }
-  } // end setToggles
-
-	// function to ensure zero values show up as placeholder in inputs
-	function resetZeroValues() {
-		for (var i = 0; i < self.itemArray.length; i++) {
-			if(self.itemArray[i].item_amount === 0) {
-				self.itemArray[i].item_amount = null;
-			}
-		}
-	} // end resetZeroValues
-
-	self.getFunctionalItems = function() {
-		console.log("getFunctionalItems is clicked");
-		budgetFactory.getFunctionalItems(self.itemArray);
-	};
-
-	self.updateFunctionalItems = function() {
-		console.log("update functional clicked");
-		setInactiveValuesToZero();
-		budgetFactory.updateFunctionalItems(self.itemArray).then(function(result){
-			switch (self.budget.budget_status) {
-        case "Finished":
-        budgetFactory.updateBudgetStatus("Finished");
-        break;
-        case "Comments":
-        budgetFactory.updateBudgetStatus("Comments");
-        break;
-        case "Financial":
-        budgetFactory.updateBudgetStatus("Financial");
-        break;
-        case "Functional":
-        budgetFactory.updateBudgetStatus("Functional");
-        break;
-        default:
-        budgetFactory.updateBudgetStatus("Functional");
-      }
-		});
-	};
-
-	self.addFunctionalItem = function() {
-		if(self.newCategory.item_name === null || self.newCategory.item_amount === null) {
-			alert('Please enter an additional category before submitting.');
-		} else {
-			console.log(self.newCategory);
-			self.newCategory.activeItem = true;
-			self.newCategory.item_img_src = 'additional.svg';
-			self.newCategory.item_sort_sequence = self.itemArray.length + 2;
-			self.newCategory.budget_template_category_id = 4;
-			self.itemArray.push(self.newCategory);
-			self.newCategory = {};
-		}
-	};
-
-	function setInactiveValuesToZero() {
-    for ( i = 0; i < self.itemArray.length; i++) {
-      if(self.itemArray[i].activeItem === false) {
-        self.itemArray[i].item_amount = 0;
-      }
-    }
-  }
 
 }]);
--- public/scripts/controller/home.controller.js
+++ public/scripts/controller/home.controller.js
@@ -1,65 +1,64 @@
 app.controller("HomeController", ["$http", "AuthFactory", "TemplateFactory", "AdminFactory", "BudgetFactory", function($http, AuthFactory, TemplateFactory, AdminFactory, BudgetFactory) {
-  console.log('HomeController started');
+    // console.log('HomeController started');
 
-  var self = this;
-  var authFactory = AuthFactory;
-  var adminFactory = AdminFactory;
-  var templateFactory = TemplateFactory;
-  var budgetFactory = BudgetFactory;
-  self.navActive = false;
+    var self = this;
+    var authFactory = AuthFactory;
+    var adminFactory = AdminFactory;
+    var templateFactory = TemplateFactory;
+    var budgetFactory = BudgetFactory;
+    self.navActive = false;
 
-  templateFactory.loadTemplateData();
-  adminFactory.getAdminData();
+    templateFactory.loadTemplateData();
+    adminFactory.getAdminData();
 
-  self.activateMobileNav = function() {
-    if(self.navActive === false){
-      self.navActive = true;
-    } else {
-      self.navActive = false;
-    }
-  };
+    self.activateMobileNav = function() {
+        if (self.navActive === false) {
+            self.navActive = true;
+        } else {
+            self.navActive = false;
+        }
+    };
 
-  // Function to Login
-  self.logIn = function() {
-    authFactory.logIn()
-    .then(function(currentUser) {
-      authFactory.idToken = currentUser.idToken;
-      self.isUserLoggedIn = true;
-      authFactory.isUserLoggedIn = self.isUserLoggedIn;
-      if(authFactory.isNewUser() === true){
-        window.location = '/#/clientprofile';
-      } else {
-        budgetFactory.getBudget().then(function(result) {
-          console.log(result);
-          var budget = result;
-          switch (budget.budget_status) {
-            case "Finished":
-            window.location = "/#/results";
-            break;
-            case "Comments":
-            window.location = "/#/results";
-            break;
-            case "Financial":
-            window.location = "/#/additionalinfo";
-            break;
-            case "Functional":
-            window.location = "/#/financialspend";
-            break;
-            case "Flow":
-            window.location = "/#/functionalspend";
-            break;
-            case "Flex":
-            window.location = "/#/flowspend";
-            break;
-            case "Profile":
-            window.location = "/#/flexspend";
-            break;
-            default:
-            window.location = "/#/clientprofile";
-          }
-        });
-      }
-    });
-  }; // End Login
+    // Function to Login
+    self.logIn = function() {
+        authFactory.logIn()
+            .then(function(currentUser) {
+                authFactory.idToken = currentUser.idToken;
+                self.isUserLoggedIn = true;
+                authFactory.isUserLoggedIn = self.isUserLoggedIn;
+                if (authFactory.isNewUser() === true) {
+                    window.location = '/#/clientprofile';
+                } else {
+                    budgetFactory.getBudget().then(function(result) {
+                        var budget = result;
+                        switch (budget.budget_status) {
+                            case "Finished":
+                                window.location = "/#/results";
+                                break;
+                            case "Comments":
+                                window.location = "/#/results";
+                                break;
+                            case "Financial":
+                                window.location = "/#/additionalinfo";
+                                break;
+                            case "Functional":
+                                window.location = "/#/financialspend";
+                                break;
+                            case "Flow":
+                                window.location = "/#/functionalspend";
+                                break;
+                            case "Flex":
+                                window.location = "/#/flowspend";
+                                break;
+                            case "Profile":
+                                window.location = "/#/flexspend";
+                                break;
+                            default:
+                                window.location = "/#/clientprofile";
+                        }
+                    });
+                }
+            });
+    }; // End Login
 
 }]); // END: NavController
